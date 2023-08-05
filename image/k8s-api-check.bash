e6257979ba4fb192009466db3f01a42eb85e75ca
Krzysztof Pudłowski <djkormo@gmail.com>
2023-08-05 23:27:56 +0200

    Update k8s-api-check.bash

--- image/k8s-api-check.bash
+++ image/k8s-api-check.bash
@@ -3,7 +3,7 @@
 # Point to the internal API server hostname
 APISERVER=https://kubernetes.default.svc
 APISERVER=$KUBERNETES_PORT_443_TCP_ADDR:$KUBERNETES_PORT_443_TCP_PORT # for AKS
-APISERVER=10.0.0.1:$KUBERNETES_PORT_443_TCP_PORT # for AKS ?
+APISERVER=https://10.0.0.1:$KUBERNETES_PORT_443_TCP_PORT # for AKS ?
 # Path to ServiceAccount token
 SERVICEACCOUNT=/var/run/secrets/kubernetes.io/serviceaccount
 
@@ -24,5 +24,5 @@ echo "TOKEN: $TOKEN"
 # Explore the API with TOKEN
 curl -v --cacert ${CACERT} --header "Authorization: Bearer ${TOKEN}" -X GET ${APISERVER}/api
 # getting node name with Token
-curl --cacert ${CACERT} --header "Authorization: Bearer ${TOKEN}" -X GET ${APISERVER}/api/nodes/$HOSTNAME
+curl -v --cacert ${CACERT} --header "Authorization: Bearer ${TOKEN}" -X GET ${APISERVER}/api/v1/nodes/$HOSTNAME
 
