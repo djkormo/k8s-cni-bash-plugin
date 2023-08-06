1b2c64f868c8a0b6a2e0a1b0953ecd8938f0a4da
Krzysztof Pudłowski <djkormo@gmail.com>
2023-08-06 20:52:19 +0200

    Update start.sh

--- image/start.sh
+++ image/start.sh
@@ -8,7 +8,7 @@ cp 10-k8s-cni-bash-plugin.conf /etc/cni/net.d/10-k8s-cni-bash-plugin.conf
 
 cp k8s-cni-bash-plugin.bash /opt/cni/bin/k8s-cni-bash-plugin
 chmod +x /opt/cni/bin/k8s-cni-bash-plugin
-"====== Installed cni plugings ==========="
+echo "====== Installed cni plugings ==========="
 ls -la  /opt/cni/bin/
-"====== Installed cni plugings ==========="
+echo "====== Installed cni plugings ==========="
 
