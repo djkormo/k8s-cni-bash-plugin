ac845b76c445d696adb8c7febba0983f38a8e845
Krzysztof Pudłowski <djkormo@gmail.com>
2023-08-06 17:11:05 +0200

    Update start.sh

--- image/start.sh
+++ image/start.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 echo "Initialising CNI bash plugin"
-
+cp 10-k8s-cni-bash-plugin.conf /etc/cni/net.d/10-k8s-cni-bash-plugin.conf
+cp k8s-cni-bash-plugin.bash /opt/cni/bin/k8s-cni-bash-plugin
+chmod +x /opt/cni/bin/k8s-cni-bash-plugin
 
