4d12970e69bbe8d26b824fdbfd074b0bff37518b
Krzysztof Pudłowski <djkormo@gmail.com>
2023-08-08 23:45:10 +0200

    Update start.sh

--- image/start.sh
+++ image/start.sh
@@ -6,10 +6,10 @@ echo "======== Configuration ========="
 cat 10-k8s-cni-bash-plugin.conf
 echo "======== Configuration ========="
 
-cp /app/10-k8s-cni-bash-plugin.conf /etc/cni/net.d/10-k8s-cni-bash-plugin.conf
+cp /cni/10-k8s-cni-bash-plugin.conf /etc/cni/net.d/10-k8s-cni-bash-plugin.conf
 
 echo "====== Installed cni plugings ==========="
-cp /app/k8s-cni-bash-plugin.bash /opt/cni/bin/k8s-cni-bash-plugin
+cp /cni/k8s-cni-bash-plugin.bash /opt/cni/bin/k8s-cni-bash-plugin
 chmod +x /opt/cni/bin/k8s-cni-bash-plugin
 
 ls -la  /opt/cni/bin/
