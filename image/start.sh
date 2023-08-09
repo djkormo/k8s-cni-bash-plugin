9cf3ee640579b1e35f5e9c513c6b6943ed75a630
Krzysztof Pudłowski <djkormo@gmail.com>
2023-08-09 22:37:50 +0200

    Update start.sh

--- image/start.sh
+++ image/start.sh
@@ -21,7 +21,7 @@ echo "========== Checking log file $LOGFILE =========="
 
 brctl addbr cni0
 ip link set cni0 up
-ip addr add 10.244.0.1/24 dev cni0
+ip addr add 10.245.0.1/24 dev cni0
 #These commands create the bridge, enable it, and then assign an IP address to it. The last command also implicitly creates a route, so that all traffic with the destination IP belonging to the pod CIDR range, local to the current node, will be redirected to the cni0 network interface. (As mentioned before, all the other software communicates with a bridge as though it were an ordinary network interface.) You can view this implicitly created route by running the ip route command from both master and worker VMs:
 
 ip route | grep cni0
