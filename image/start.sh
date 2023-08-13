5028589dd798ba20e895a189006bc34ff5c8c6dc
Krzysztof Pudłowski <djkormo@gmail.com>
2023-08-13 13:34:18 +0200

    Update start.sh

--- image/start.sh
+++ image/start.sh
@@ -3,23 +3,24 @@
 
 echo "Initialising CNI bash plugin"
 echo "PATH: ${PATH}"
+
 echo "======== Configuration ========="
 cat /cni/10-k8s-cni-bash-plugin.conf
-echo "======== Configuration ========="
 export $(cat 10-k8s-cni-bash-plugin.env)
+printenv | grep CNI
+echo "======== Configuration ========="
 
 cp /cni/10-k8s-cni-bash-plugin.conf /etc/cni/net.d/10-k8s-cni-bash-plugin.conf
 
 echo "====== Installed cni plugings ==========="
 cp /cni/k8s-cni-bash-plugin.bash /opt/cni/bin/k8s-cni-bash-plugin
 chmod +x /opt/cni/bin/k8s-cni-bash-plugin
-
 ls -la  /opt/cni/bin/
 echo "====== Installed cni plugings ==========="
 
-echo "========== Checking log file $LOGFILE =========="
-tail ${LOGFILE}
-echo "========== Checking log file $LOGFILE =========="
+echo "========== Checking log file ${CNI_LOGFILE} =========="
+tail ${CNI_LOGFILE}
+echo "========== Checking log file ${CNI_LOGFILE} =========="
 
 # Create bridge only if it doesn't yet exist
 if ! ip link show cni0 &>/dev/null; then
