294471db0d0474cb607770aa1ae355510f0c61c8
Krzysztof Pudłowski <djkormo@gmail.com>
2023-08-13 18:27:16 +0200

    Update cni-add-check.bash

--- image/cni-add-check.bash
+++ image/cni-add-check.bash
@@ -4,8 +4,11 @@
 export $(cat k8s-cni-bash-plugin.env)
 
 #cat /etc/cni/net.d/10-k8s-cni-bash-plugin.conf | CNI_COMMAND=ADD CNI_CONTAINERID=example CNI_NETNS=/dev/null CNI_IFNAME=dummy0 CNI_PATH=/opt/cni/bin /opt/cni/bin/k8s-cni-bash-plugin
-
-cat /etc/cni/net.d/10-k8s-cni-bash-plugin.conf | DEBUG=1 CNI_COMMAND=ADD CNI_CONTAINERID=example CNI_NETNS=/dev/null CNI_IFNAME=dummy0 CNI_PATH=/opt/cni/bin /opt/cni/bin/k8s-cni-bash-plugin
+rand=$(tr -dc 'A-F0-9' < /dev/urandom | head -c4)
+host_if_name="veth$rand"
+rand=$(tr -dc '0-9' < /dev/urandom | head -c4)
+host_ns_name="$rand"
+cat /etc/cni/net.d/10-k8s-cni-bash-plugin.conf | DEBUG=1 CNI_COMMAND=ADD CNI_CONTAINERID=example CNI_NETNS=/dev/null CNI_IFNAME=host_if_name CNI_PATH=/opt/cni/bin /opt/cni/bin/k8s-cni-bash-plugin
 
 tail /var/log/cni.log
 
