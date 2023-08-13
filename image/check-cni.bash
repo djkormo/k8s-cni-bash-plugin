210cfd60a62e8fa966db8c043f326283ec7f278d
Krzysztof Pudłowski <djkormo@gmail.com>
2023-08-13 14:03:18 +0200

    Update check-cni.bash

--- image/check-cni.bash
+++ image/check-cni.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #cho '{ "cniVersion": "0.3.1", "name": "k8s-cni-bash-plugin","type": "k8s-cni-bash-plugin","network": "10.240.0.0/16", "podcidr": "10.240.0.0/24" }' | CNI_COMMAND=ADD CNI_CONTAINERID=example CNI_NETNS=/dev/null CNI_IFNAME=dummy0 CNI_PATH=. /opt/cni/bin/k8s-cni-bash-plugin
-export $(cat 10-k8s-cni-bash-plugin.env)
+export $(cat k8s-cni-bash-plugin.env)
 
 cat /etc/cni/net.d/10-k8s-cni-bash-plugin.conf | CNI_COMMAND=ADD CNI_CONTAINERID=example CNI_NETNS=/dev/null CNI_IFNAME=dummy0 CNI_PATH=/opt/cni/bin /opt/cni/bin/k8s-cni-bash-plugin
 
