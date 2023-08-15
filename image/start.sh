bfe1ee4a5c61c8863d4789e7a4b5991a0a33e140
Krzysztof Pudłowski <djkormo@gmail.com>
2023-08-15 16:14:51 +0200

    Update start.sh

--- image/start.sh
+++ image/start.sh
@@ -10,7 +10,7 @@ node_number=$(($node_number))
 echo "Node $CNI_HOSTNAME number: ${node_number}"
 
 echo "======== Configuration ========="
-export $(cat k8s-cni-bash-plugin.env)
+#export $(cat k8s-cni-bash-plugin.env)
 printenv | grep CNI
 echo "======== Configuration ========="
 # included via configmap
@@ -26,8 +26,8 @@ chmod +x /opt/cni/bin/k8s-cni-bash-plugin
 ls -la  /opt/cni/bin/
 echo "====== Installed cni plugings ==========="
 
-#echo "========== Checking log file ${CNI_LOGFILE} =========="
-#tail ${CNI_LOGFILE}
+echo "========== Checking log file ${CNI_LOGFILE} =========="
+tail ${CNI_LOGFILE} -n 200
 #echo "========== Checking log file ${CNI_LOGFILE} =========="
 
 ip route | grep cni0
