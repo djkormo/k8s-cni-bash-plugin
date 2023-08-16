dc93943bebe821fe090ad50d1085384c276d0815
Krzysztof Pudłowski <djkormo@gmail.com>
2023-08-16 14:17:28 +0200

    Update k8s-cni-bash-plugin.bash

--- image/k8s-cni-bash-plugin.bash
+++ image/k8s-cni-bash-plugin.bash
@@ -119,11 +119,14 @@ ADD)
 
       # Set up NAT for traffic leaving the cluster (replace Pod IP with node IP)
       logger "Set up NAT for traffic leaving the cluster (replace Pod IP with node IP): $pod_cidr -> $host_network"
+      
       # TODO Not working YET
-      
       logger "iptables -t nat -N $my_cni_masquerade &>/dev/null"
-      #iptables -t nat -N "$my_cni_masquerade" &>/dev/null
-      
+      if ! iptables -t nat -N "$my_cni_masquerade" &>/dev/null; then
+        iptables -t nat -N "$my_cni_masquerade" &>/dev/null
+      else
+        logger "Not needed to add chain : $my_cni_masquerade "
+      fi
       logger "iptables -t nat -A $my_cni_masquerade -d $host_network -j RETURN"
       ensure iptables -t nat -A "$my_cni_masquerade" -d "$host_network" -j RETURN
       
