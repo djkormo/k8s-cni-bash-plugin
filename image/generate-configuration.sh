99fb3c767971b5748219db92835689429c9b0439
Krzysztof Pudłowski <djkormo@gmail.com>
2023-08-24 23:49:14 +0200

    Update generate-configuration.sh

--- image/generate-configuration.sh
+++ image/generate-configuration.sh
@@ -40,7 +40,7 @@ fi
 # TODO
 # Listening all nodes, numbering tham if spec.podCIDR is not set 
 
-node_names=$(curl --cacert "${KUBE_CACERT}" --header "Authorization: Bearer ${SERVICEACCOUNT_TOKEN}" -X GET "${KUBERNETES_SERVICE_PROTOCOL}://${KUBERNETES_SERVICE_HOST}:${KUBERNETES_SERVICE_PORT}/api/v1/nodes/" | jq -rM '.items[].metadata.name' )
+node_names=$(curl --cacert "${KUBE_CACERT}" --header "Authorization: Bearer ${SERVICEACCOUNT_TOKEN}" -X GET "${KUBERNETES_SERVICE_PROTOCOL}://${KUBERNETES_SERVICE_HOST}:${KUBERNETES_SERVICE_PORT}/api/v1/nodes/" | jq  '.items[].metadata.name' )
 
 mapfile -t nodenumber < <( echo $node_names )
 
