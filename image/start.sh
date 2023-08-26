c70dee48adc6668b9174492650144a6a8ef88762
Krzysztof Pudłowski <djkormo@gmail.com>
2023-08-26 15:59:09 +0200

    Update start.sh

--- image/start.sh
+++ image/start.sh
@@ -49,7 +49,7 @@ fi
 
 node_names=$(curl --cacert "${KUBE_CACERT}" --header "Authorization: Bearer ${SERVICEACCOUNT_TOKEN}" -X GET "${KUBERNETES_SERVICE_PROTOCOL}://${KUBERNETES_SERVICE_HOST}:${KUBERNETES_SERVICE_PORT}/api/v1/nodes/" | jq -rM '.items[] |"\(.metadata.name) \(.spec.podCIDR)"' )
 
-echo "node_names: $node_names"
+echo "node_names:\n$node_names"
 
 #mapfile -t nodenumber < <( echo "$node_names" )
 
